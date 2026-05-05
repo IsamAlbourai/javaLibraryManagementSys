c3d57f1ff0978ba67799f0cb2868cdbd68ad8a14
Isam <123lightningstrike@gmail.com>
2026-05-05 11:41:31 +0400

    Added Library Service Class

--- src/LibraryService.java
+++ src/LibraryService.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class g {
+public class LibraryService {
+    private ArrayList<LibraryItem> items = new ArrayList<>();
+
+    public void addItem(LibraryItem item) {
+        items.add(item);
+    }
+
+    public LibraryItem findItemById(int id) {
+        for (LibraryItem item : items) {
+            if (item.getId() == id)
+                return item;
+        }
+        return null;
+    }
+
+    public void listAvailableItems() {
+        for (LibraryItem item : items) {
+            if (item.isAvailable()) {
+                System.out.println(item.getDetails());
+            }
+        }
+    }
+
+    public void searchByTitle(String keyword) {
+        for (LibraryItem item : items) {
+            if (item.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+                System.out.println(item.getDetails());
+            }
+        }
+    }
 }
