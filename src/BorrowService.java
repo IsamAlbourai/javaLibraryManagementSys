22bd8f64cddbb1065dce5c63da4ca9ebc403c134
Isam <123lightningstrike@gmail.com>
2026-05-05 12:51:49 +0400

    Added Borrow Service Class

---
+++ src/BorrowService.java
@@ -0,0 +1,42 @@
+public class BorrowService {
+    private LibraryService libraryService;
+    private MemberService memberService;
+
+    public BorrowService(LibraryService lib, MemberService mem) {
+        this.libraryService = lib;
+        this.memberService = mem;
+    }
+
+    public void borrowItem(int memberId, int itemId) {
+        Member member = memberService.findMemberById(memberId);
+        LibraryItem item = libraryService.findItemById(itemId);
+
+        if (member == null || item == null) {
+            System.out.println("Member or Item not found.");
+            return;
+        }
+
+        if (!item.isAvailable()) {
+            System.out.println("Item already borrowed.");
+            return;
+        }
+
+        item.borrowItem();
+        member.borrowItem(item);
+        System.out.println("Item borrowed successfully.");
+    }
+
+    public void returnItem(int memberId, int itemId) {
+        Member member = memberService.findMemberById(memberId);
+        LibraryItem item = libraryService.findItemById(itemId);
+
+        if (member == null || item == null) {
+            System.out.println("Member or Item not found.");
+            return;
+        }
+
+        item.returnItem();
+        member.returnItem(item);
+        System.out.println("Item returned successfully.");
+    }
+}
